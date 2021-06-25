e7ee1b4871e177d2cafb7a4db4bccbbd652be40c
Jan Erik <60162470+jnaess@users.noreply.github.com>
2021-06-25 18:08:10 +0000

    landing page and student and cafe landing pages

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-from flask import Flask
-from flask import request, render_template
+from flask import Flask, render_template
 
-from joblib import load
-import numpy as np
+# Create a flask app
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
-app = Flask(__name__)
 
+# Index page (now using the index.html file)
 @app.route('/')
 def index():
-    return render_template('index.html')
+  """
+  This is the home page. 
+  login option (either as a cafe or as a customer)
+  """
+  return render_template('index.html')
 
+@app.route('/cafe')
+def cafe():
+  """
+  home page for cafe (automatically cafe dashboard)
+  """
+  return render_template("cafe.html")
+
+@app.route('/customer')
+def customer():
+  """
+  home page for customer. they should be able to order directly from here
+  """
+  return render_template("customer.html")
+  
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
-
+    # Run the Flask app
+    app.run(host='0.0.0.0', debug=True, port=8080)
