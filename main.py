aff53c73624cc2fc39e76c68323dfaaec862805c
Jan Erik <60162470+jnaess@users.noreply.github.com>
2021-06-27 00:27:15 +0000

    multiple tables makable. Now committing to take offline and add test database

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from forms import SignUpForm
 import numpy as np
 import pandas as pd
 
 # Create a flask app
 app = Flask(__name__, template_folder='templates', static_folder='static')
-
 
 # Index page (now using the index.html file)
 @app.route('/', methods=("POST", "GET"))
@@ -20,27 +20,23 @@ def index():
 @app.route('/cafe')
 def cafe():
     """
-  This is the cafe dashboard
+  Displays cafe dashboard (loads today's orders)
+
+  TODO:
+  -hidden option for delviered orders
+  -transfer orders
+  -pop up for more order details
+
+  add link to each column in a table --> https://stackoverflow.com/questions/59438723/flask-make-row-of-dataframe-to-html-clickable
   """
+    #queue all orders for the day
+    unmade, undelivered, delivered = retrieve_orders()
 
-    uncompleted = pd.DataFrame({
-        "name": ["Ms. J", "Ms. Katharine", "Mr. Coolio"],
-        "period": [2, 2, 3]
-    })
-
-    completed = pd.DataFrame({
-        "name": ["Ms. Jj", "Ms. Kk", "Mr. Cc"],
-        "period": [1, 1, 2]
-    })
-
-    #this will be loaded as a query for all
-    #orders = order_querry
-
+    #currently delivered orders are not displayed
     return render_template(
         "cafe.html",
-        uncompleted=[uncompleted.to_html(classes='data', header='true')],
-        completed=[completed.to_html(classes='data', header='true')])
-
+        unmade=[unmade.to_html(classes='data', header='true')],
+        undelivered=[undelivered.to_html(classes='data', header='true')])
 
 @app.route('/manager')
 def manager():
@@ -49,6 +45,24 @@ def manager():
   """
     return render_template("manager.html")
 
+@app.route('/user')
+def user():
+    """
+  The dashboard for the cafe manager
+  """
+    return render_template("user.html")
+
+@app.route('/signup', methods = ['POST', 'GET'])
+def signup():
+    """
+  test manager form
+  """
+
+    form = SignUpForm()
+    if form.is_submitted():
+      result = request.form
+      return render_template('user.html', result = result)
+    return render_template("signup.html", form=form)
 
 @app.route('/customer')
 def customer():
@@ -57,6 +71,26 @@ def customer():
   """
     return render_template("customer.html")
 
+def retrieve_orders():
+    """
+  Retrieves all orders for the current day. Splits them into a dataframe of unmade, undelivered, and delivered orders
+
+  input: none
+  returns: 
+    unmade, df
+    undelivered, df
+    delivered, df
+  """
+    unmade = pd.DataFrame({
+        "name": ["Ms. J", "Ms. Katharine", "Mr. Coolio"],
+        "period": [2, 2, 3]
+    })
+
+    undelivered = pd.DataFrame({"name": ["Mr. Cc"], "period": [2]})
+
+    delivered = pd.DataFrame({"name": ["Ms. Jj", "Ms. Kk"], "period": [1, 1]})
+
+    return unmade, undelivered, delivered
 
 if __name__ == '__main__':
     # Run the Flask app
